a5b13482b7050f9870ecc2c45caa4b8de37c21e3
om <omsukalkar@gmail.com>
2026-04-21 06:46:12 +0530

    get

--- src/main/java/com/Contact/controller/Controller.java
+++ src/main/java/com/Contact/controller/Controller.java
@@ -1,6 +1,7 @@
 package com.Contact.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,11 @@ import com.Contact.service.UserService;
 public class Controller {
 	@Autowired
 	private UserService userServ;
+	
+	@GetMapping("/")
+	public String home() {
+	    return "App is running 🚀";
+	}
 	
 	@PostMapping("/add")
 	public void add( @RequestBody Users user)
